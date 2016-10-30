de803c517e2fbe6efa676521e8380962a2cdd141
Taylor Flagg <trflagg@gmail.com>
2016-10-30 11:34:37 -0400

    Fix development home url in config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://localhost:2368',
+        url: 'http://192.168.99.100:2368',
 
         // Example refferer policy
         // Visit https://www.w3.org/TR/referrer-policy/ for instructions
